7ce4abe4f63963e2065f5d1632d544054e0a6a7b
L. Hebert <lucyhebert@free.fr>
2017-06-18 15:19:54 +0200

    Implémentation étape 1

--- src/main/java/com/javaEE/mysql/MysqlApplication.java
+++ src/main/java/com/javaEE/mysql/MysqlApplication.java
@@ -2,8 +2,10 @@ package com.javaEE.mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.javaEE.mysql")
 public class MysqlApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/javaEE/mysql/UtilisateurRepository.java
@@ -0,0 +1,10 @@
+package com.javaEE.mysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Lucy on 18/06/2017.
+ */
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, String> {
+    
+}
